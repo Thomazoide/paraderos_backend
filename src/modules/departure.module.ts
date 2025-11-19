09a784b244096f867bd0d226a2e60af80a660c91
Thomazoide <ttellerias01@outlook.com>
2025-11-19 13:56:19 -0300

    new files

--- src/modules/departure.module.ts
+++ src/modules/departure.module.ts
@@ -1,4 +1,20 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DepartureController } from "src/controllers/departure.controller";
+import { Departure } from "src/entities/departure.entity";
+import { DepartureService } from "src/services/departure.service";
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            Departure
+        ])
+    ],
+    controllers: [
+        DepartureController
+    ],
+    providers: [
+        DepartureService
+    ]
+})
 export class DepartureModule {};
